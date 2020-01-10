1e8a6abb66f4712c3287f5c377e92cd737f32310
Catherine Smiley <catherinesmiley@gmail.com>
2020-01-10 08:34:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,53 @@
+# class Dog 
+#   def initialize(name, breed = "Mutt")
+#     @name = name
+#     @breed = breed
+#   end
+  
+#   # def name=(name)
+#   #   @name = name
+#   # end
+  
+#   # def name
+#   #   @name
+#   # end
+  
+#   # def breed=(breed)
+#   #   @breed = breed
+#   # end
+  
+#   # def breed
+#   #   @breed
+#   # end
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Dog 
   def initialize(name, breed = "Mutt")
-    @name = name
-    @breed = breed
+    @name = name 
+    @breed = breed 
   end
   
-  def name=(name)
-    @name = name
+  def name 
+    @name 
   end
   
-  def name
-    @name
+  def breed 
+    @breed 
   end
   
-  def breed=(breed)
-    @breed = breed
-  end
-  
-  def breed
-    @breed
-  end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,40 @@
-class Person
+# class Person
+#   def initialize(name)
+#     @name = name
+#   end
+  
+#   def name=(name)
+#     @name = name
+#   end
+  
+#   def name
+#     @name
+#   end
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Person 
   def initialize(name)
-    @name = name
+    @name = name 
   end
   
-  def name=(name)
-    @name = name
+  def name 
+    @name 
   end
   
-  def name
-    @name
-  end
 end
